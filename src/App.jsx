bed8bec5c0d8ccc89f13f2033b65415bc4c898de
AndreaRaga56 <andrearaga56@gmail.com>
2024-12-23 22:21:55 +0100

    del debug

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     setTitle("")
   }
 
-  const remove = (id) =>{
+  const removePost = (id) =>{
     const newListaPosts= listaPosts.filter((curElem, index)=>{
       return index!=id
     })
@@ -22,7 +22,7 @@ function App() {
   const printPosts = listaPosts.map((curPost, i) => {
     return <div key={i} className="d-flex align-items-center mt-5 gap-1">
       <div className="post-card col">{curPost}</div>
-      <div><button onClick={()=>{remove(i)}} className="btn btn-danger">🗑</button></div>
+      <div><button onClick={()=>{removePost(i)}} className="btn btn-danger">🗑</button></div>
     </div>
   })
 
@@ -31,21 +31,22 @@ function App() {
     <>
       <div className="container mt-5">
         <h1>Il mio Blog</h1>
+
+        {/* FORM */}
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="newPost" className="form-label">Aggiungi un nuovo post</label>
             <input type="text" required maxLength={25} className="form-control" id="newPost" value={title} onChange={(event) => setTitle(event.target.value)} />
-            <div>{title}</div>
           </div>
           <button type="submit" className="btn btn-primary">Inserisci</button>
         </form>
+
+        {/* TITOLO DEI POSTS */}
         <div className="text-center">
           <div className="row row-cols-3">
             {printPosts}
           </div>
-
         </div>
-
 
       </div>
     </>
