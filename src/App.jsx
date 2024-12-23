ccdb28568fdc8970b06a3acdb62c2bfe61beb665
AndreaRaga56 <andrearaga56@gmail.com>
2024-12-23 20:08:18 +0100

    feat form button, submit e post titles

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,29 @@ import { useState } from "react"
 
 function App() {
 
-  const listaPosts = [];
-
+  let [listaPosts, setListaPosts] = useState([])
   let [title, setTitle] = useState("")
+
+  const handleSubmit =(event)=>{
+    event.preventDefault()
+    let newListaPosts = [...listaPosts, title]
+    setListaPosts (newListaPosts);
+    setTitle ("")
+  }
 
   return (
     <>
       <div className="container">
         <h1>Il mio Blog</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="newPost" className="form-label">Aggiungi un nuovo post</label>
             <input type="text" className="form-control" id="newPost" value={title} onChange={(event)=>setTitle(event.target.value)} />
             <div>{title}</div>
           </div>
+          <button type="submit" className="btn btn-primary">Inserisci</button>
         </form>
+        {listaPosts}
       </div>
     </>
   )
